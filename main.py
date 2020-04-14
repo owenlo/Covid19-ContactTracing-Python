dc3fc48807414a158f7af82a9e8a667b33682aa8
owenlo <o.lo@napier.ac.uk>
2020-04-14 12:44:25 +0000

    Added prox id generation function

--- main.py
+++ main.py
@@ -8,6 +8,15 @@
 import key_schedule
 import binascii
 
+def generateProxIds(daily_trace_key):
+  """Generate all prox IDs for a 24 hour period based on Daily Trace Key"""
+  prox_ids = []
+  
+  for timeInterval in range(0, 144): #each interval is equal to 10 minutes of time.
+    prox_ids.append(key_schedule.rolling_prox_id(daily_trace_key, timeInterval))
+
+  return prox_ids
+
 if __name__ == "__main__":
   """ Simulate the Rolling Prox ID for a 24-hour window of Day Number 1. """
 
@@ -16,13 +25,12 @@ if __name__ == "__main__":
   trace_key = key_schedule.trace_key() # Begin contact tracing and generate a Trace Key
   daily_trace_key = key_schedule.daily_trace(1, trace_key) # Generate a rolling 24-hour Daily Tracing Key
 
-
   print("**********")
   print("Trace Key: " + binascii.hexlify(trace_key).decode())    
   print("Daily Trace Key: " + binascii.hexlify(daily_trace_key).decode())
   print("Day Number: " + str(dayNumber))
   print("**********")
+  
+  for i, prox_id in enumerate(generateProxIds(daily_trace_key)):
+    print("Time Interval: " + str(i) + "; Prox ID: " + binascii.hexlify(prox_id).decode())
 
-  for timeInterval in range(0, 144): #each interval is equal to 10 minutes of time.
-    prox_id = key_schedule.rolling_prox_id(daily_trace_key, timeInterval)  
-    print("Time Interval: " + str(timeInterval) + "; Prox ID: " + binascii.hexlify(prox_id).decode())
